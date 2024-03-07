4c246af8034f9bc5879cb5baa687113d82d13a14
LAPTOP-ICOONUPR\vissi <vissie@vistok.co.za>
2024-03-07 14:37:42 +0200

    First template for review

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="flex justify-center">
+      <img src="images/background-sky-3.png" alt="" />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Nav.jsx
@@ -0,0 +1,12 @@
+import './Nav.css';
+
+function Nav() {
+
+  return (
+    <div className="flex justify-center nav_bg">
+      <img src="images/PlayCity-logo@2x.png" alt="PlayCity" className='logo' />
+    </div>
+  )
+}
+
+export default Nav
